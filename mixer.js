4a84877041b09ebcae9bd8610ced9bd2c4d2c23e
ksvcr <ksv.crowley@gmail.com>
2017-08-29 00:51:00 +0500

    mixer

---
+++ mixer.js
@@ -0,0 +1,24 @@
+function mixer(arr) {
+  var j = arr.length;
+  var i;
+  var temp;
+
+  while (j > 0) {
+    i = Math.floor(Math.random() * j);
+    console.log(i);
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp; 
+
+    if (arr[i] > arr[i+1] && arr[j] > arr[j+1]) {
+      j--;
+    }
+  }
+
+
+
+  return arr;
+}
+
+var arr = [1, 68, 213, 1, 59, 394, 43, 12, 1, 324, 1, 999, 1];
+console.log(mixer(arr));
